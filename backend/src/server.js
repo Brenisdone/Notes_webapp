7da022887bb772ce5f042342b904a967a6ef4ac5
Brenisdone <fernandes.brendon69@gmail.com>
2026-03-03 11:46:08 +0530

    finished homepage

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 //const express = require("express"); => for type:commonjs
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
 import router from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import ratelimiter from "./middleware/ratelimiter.js";
 
 dotenv.config();
@@ -11,8 +13,13 @@ const app = express();
 const PORT = process.env.port || 5001
 
 //middleware
+//only cors()=>allows request by any urls
+app.use(cors(
+    {origin: "http://localhost:5173"}
+));
 app.use(express.json()); //parses JSON bodies: req.body
 app.use(ratelimiter);
+
 //simple custom middleware
 // app.use((req,res,next) => {
 //     console.log(`Request recieved, Method: ${req.method}, URL: ${req.url}`);
